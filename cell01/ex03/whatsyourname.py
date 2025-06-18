804de0fe73c43a4cab51be1e4b0907c058db0cc6
jhembuak <Fjrpxdprs@gmail.com>
2025-06-18 12:07:31 +0700

    Create whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Hey, what's your first name :Jiraporn ").strip()
+last_name = input("And your last name :Hembuaklad ").strip()
+print("Well, pleass to meet you, Jiraporn Hembuaklad.")
